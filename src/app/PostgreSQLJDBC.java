c9062fa6bde23640886d232753daf07d30babf97
Reza Lesmana <reza.lesmana@gdn-commerce.com>
2017-08-14 14:41:22 +0700

    Prepared Statement
    Prepared Statement Examples to Avoid SQL Injection

--- src/app/PostgreSQLJDBC.java
+++ src/app/PostgreSQLJDBC.java
@@ -5,9 +5,11 @@ package app;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  * @author reza.lesmana
@@ -24,15 +26,61 @@ public class PostgreSQLJDBC {
 		openConnection();
 		createTables();
 		insertDataToTables();
-		showAllLogisticProvider();
 		closeConnection();
+		showInputForm();
 	}
 
-	private static void showAllLogisticProvider() {
+	private static void showInputForm() {
+		Scanner scanner = new Scanner(System.in);
+	    System.out.print("Enter Logistic Code : ");
+	    String logisticCode = scanner.nextLine();
+	    scanner.close();
+	    openConnection();
+	    showLogisticProvider(logisticCode);
+	    closeConnection();
+	}
+
+	private static void showLogisticProvider(String logisticCode) {
+		showLogisticProviderUsingStatement(logisticCode);
+		showLogisticProviderUsingPreparedStatement(logisticCode);
+	}
+
+	private static void showLogisticProviderUsingPreparedStatement(String logisticCode) {
+		System.out.println("Using Prepared Statement to get Logistic Provider " + logisticCode);
+		PreparedStatement stmt = null;
+		try {
+			String sql = "SELECT * FROM LOGISTIC_PROVIDER WHERE LOGISTIC_CODE = ?";
+			stmt = c.prepareStatement(sql);
+			stmt.setString(1, logisticCode);
+			ResultSet rs = stmt.executeQuery();
+			while ( rs.next() ) {
+	            int id = rs.getInt("id");
+	            String  code = rs.getString("logistic_code");
+	            String name  = rs.getString("logistic_name");
+	            float fee = rs.getFloat("fee");
+	            float discount = rs.getFloat("discount");
+	            System.out.println( "ID = " + id );
+	            System.out.println( "CODE = " + code );
+	            System.out.println( "NAME = " + name );
+	            System.out.println( "FEE = " + fee );
+	            System.out.println( "DISCOUNT = " + discount );
+	            System.out.println();
+	         }
+	         rs.close();
+	         stmt.close();
+		} catch(Exception e) {
+			System.out.println("Failure on using prepared statement : " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private static void showLogisticProviderUsingStatement(String logisticCode) {
+		System.out.println("Using Statement to get Logistic Provider " + logisticCode);
 		Statement stmt = null;
 		try {
 			stmt = c.createStatement();
-			ResultSet rs = stmt.executeQuery( "SELECT * FROM LOGISTIC_PROVIDER;" );
+			String sql = "SELECT * FROM LOGISTIC_PROVIDER WHERE LOGISTIC_CODE = '" + logisticCode + "'";
+			ResultSet rs = stmt.executeQuery(sql);
 	         while ( rs.next() ) {
 	            int id = rs.getInt("id");
 	            String  code = rs.getString("logistic_code");
@@ -43,13 +91,13 @@ public class PostgreSQLJDBC {
 	            System.out.println( "CODE = " + code );
 	            System.out.println( "NAME = " + name );
 	            System.out.println( "FEE = " + fee );
-	            System.out.println( "discount = " + discount );
+	            System.out.println( "DISCOUNT = " + discount );
 	            System.out.println();
 	         }
 	         rs.close();
 	         stmt.close();
 		} catch(Exception e) {
-			System.out.println("Failure on selecting data from tables : " + e.getMessage());
+			System.out.println("Failure on using statement : " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
