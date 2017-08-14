e9d6b8e7d8cd26f76d1f8d96d3a6a8a89d845389
Reza Lesmana <reza.lesmana@gdn-commerce.com>
2017-08-14 13:05:14 +0700

    Create Table
    Create Table Logistic Provider using JDBC

--- src/app/PostgreSQLJDBC.java
+++ src/app/PostgreSQLJDBC.java
@@ -5,18 +5,54 @@ package app;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author reza.lesmana
  *
  */
 public class PostgreSQLJDBC {
+	
+	private static Connection c = null;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Connection c = null;
+		openConnection();
+		createTables();
+		closeConnection();
+	}
+
+	private static void closeConnection() {
+        try {
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void createTables() {
+		Statement stmt = null;
+		try {
+			stmt = c.createStatement();
+			String sql = "CREATE TABLE LOGISTIC_PROVIDER " +
+		            "(ID INT PRIMARY KEY     NOT NULL," +
+		            " LOGISTIC_CODE           TEXT    NOT NULL, " +
+		            " LOGISTIC_NAME           TEXT     NOT NULL, " +
+		            " FEE        REAL, " +
+		            " DISCOUNT REAL)";
+	         stmt.executeUpdate(sql);
+	         stmt.close();
+	         System.out.println("Finish creating tables");
+		} catch(Exception e) {
+			System.out.println("Failure on creating tables : " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private static void openConnection() {
 		try {
 			Class.forName("org.postgresql.Driver");
 			c = DriverManager.getConnection(
