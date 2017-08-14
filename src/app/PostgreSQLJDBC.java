1d0035dc169551048e4dace40a3cba9c08810d0d
Reza Lesmana <reza.lesmana@gdn-commerce.com>
2017-08-14 13:41:59 +0700

    Insert Data & ResultSet
    How to Insert Data & ResultSet Usage Example

--- src/app/PostgreSQLJDBC.java
+++ src/app/PostgreSQLJDBC.java
@@ -5,6 +5,7 @@ package app;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -22,13 +23,63 @@ public class PostgreSQLJDBC {
 	public static void main(String[] args) {
 		openConnection();
 		createTables();
+		insertDataToTables();
+		showAllLogisticProvider();
 		closeConnection();
+	}
+
+	private static void showAllLogisticProvider() {
+		Statement stmt = null;
+		try {
+			stmt = c.createStatement();
+			ResultSet rs = stmt.executeQuery( "SELECT * FROM LOGISTIC_PROVIDER;" );
+	         while ( rs.next() ) {
+	            int id = rs.getInt("id");
+	            String  code = rs.getString("logistic_code");
+	            String name  = rs.getString("logistic_name");
+	            float fee = rs.getFloat("fee");
+	            float discount = rs.getFloat("discount");
+	            System.out.println( "ID = " + id );
+	            System.out.println( "CODE = " + code );
+	            System.out.println( "NAME = " + name );
+	            System.out.println( "FEE = " + fee );
+	            System.out.println( "discount = " + discount );
+	            System.out.println();
+	         }
+	         rs.close();
+	         stmt.close();
+		} catch(Exception e) {
+			System.out.println("Failure on selecting data from tables : " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private static void insertDataToTables() {
+		Statement stmt = null;
+		try {
+			stmt = c.createStatement();
+			String sql = "INSERT INTO LOGISTIC_PROVIDER " +
+		            "(ID, LOGISTIC_CODE, LOGISTIC_NAME, FEE, DISCOUNT) "
+		            + "VALUES (1, 'JNE', 'PT. Tiki Jalur Nugraha Ekakurir', 15000, 0.02)";
+			String sql2 = "INSERT INTO LOGISTIC_PROVIDER " +
+		            "(ID, LOGISTIC_CODE, LOGISTIC_NAME, FEE, DISCOUNT) "
+		            + "VALUES (2, 'POS', 'PT. POS INDONESIA', 13500, 0.015)";
+	         stmt.executeUpdate(sql);
+	         stmt.executeUpdate(sql2);
+	         stmt.close();
+	         System.out.println("Finish inserting data to tables");
+		} catch(Exception e) {
+			System.out.println("Failure on inserting data to tables : " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 
 	private static void closeConnection() {
         try {
 			c.close();
+			System.out.println("Close db connection successfully");
 		} catch (SQLException e) {
+			System.out.println("Failure on closing db connection : " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
